0cee985e18de95a0a2a6b5f9bbe884df16888b25
shihabosm <shihabshamim767@gmail.com>
2024-11-09 01:39:16 +0600

    about and carosel

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,11 +1,13 @@
 import About from "../About/About"
 import Banner from "../Banner/Banner"
+import Carosel from "../Carosel/Carosel"
 
 const Home = () => {
   return (
     <div>
       <Banner />
       <About></About>
+      <Carosel></Carosel>
     </div>
   )
 }
